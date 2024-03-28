bd2329d8b177a5b920cabba82055265a1ab88755
Dania Haroon <159708973+Daniaharoon@users.noreply.github.com>
2024-03-28 10:17:04 +0500

    Add files via upload

---
+++ ques8.js
@@ -0,0 +1,12 @@
+"use strict";
+//  8. You should create four lines that look like this:
+// // console.log(5 + 3)
+// // Your output should simply be four lines with the number 8 appearing once on each line.
+//ADDITION:
+console.log(6 + 2);
+//SUBTRACTION:
+console.log(10 - 2);
+//MULTIPLICATION:
+console.log(4 * 2);
+//DIVISIon
+console.log(16 / 2);
